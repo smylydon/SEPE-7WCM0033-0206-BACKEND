4e94003eff9019e5f4c3c21cb50baea7c4a99f9b
Michael Godden <smylydon@yahoo.com>
2016-02-08 22:12:50 -0800

    Sequelize is set up and working with Postgres.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var morgan = require("morgan");
 var bodyParser = require("body-parser");
 var jwt = require("jsonwebtoken");
 var mongoose = require("mongoose");
+var Sequelize = require('sequelize');
 var app = express();
 
 var port = process.env.PORT || 3001;
@@ -11,7 +12,39 @@ var User = require('./models/User');
 console.log('MONGO_URL:', process.env.MONGO_URL);
 // Connnect to DB.
 mongoose.connect(process.env.MONGO_URL);
+sequelize = new Sequelize('mydb', 'postgres', 'password', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
 
+var Person = sequelize.define('person', {
+  first_name: Sequelize.STRING,
+  last_name: Sequelize.STRING,
+  dob: Sequelize.DATE,
+  sex: Sequelize.STRING
+}, {
+  paranoid: true,
+  underscored: true
+});
+var pgUser = sequelize.define('user', {
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+  token: Sequelize.STRING,
+}, {
+  paranoid: true,
+  underscored: true
+});
+Person.findOne().then(function(person) {
+  console.log('found person: ', person.first_name);
+});
+pgUser.findOne().then(function(user) {
+  console.log('found user: ', user);
+});
 app.use(bodyParser.urlencoded({
   extended: true
 }));
